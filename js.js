06ee4189a5f06b1e75d8cc6ee578d43ca03754ef
m v <100205275+brisasol@users.noreply.github.com>
2024-03-04 01:16:03 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -1,4 +1,11 @@
-// Initialization for ES Users
-import { Carousel, initMDB } from "mdb-ui-kit";
 
-initMDB({ Carousel });
+
+$('.carousel .carousel-item').each(function () {
+    var minPerSlide = 4;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    for (var i = 0; i < minPerSlide; i++) { next=next.next(); if (!next.length) { next=$(this).siblings(':first'); } next.children(':first-child').clone().appendTo($(this)); } });
